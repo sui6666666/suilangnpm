c3adc12b8b2613fedf507f72361bbd6309e96a4c
suilang <li66666666@hotmail.com>
2021-08-17 12:30:55 +0800

    1

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.printMsg = function() {
-  console.log("This is a message from the demo package555555555555556");
+  console.log("This is a message from the demo package5555555555555567");
 }
